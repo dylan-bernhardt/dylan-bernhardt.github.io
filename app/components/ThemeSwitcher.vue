c0e280fa100b99af9c822552dd1955efdbe73e54
dylan-bernhardt <dylan1122@outlook.fr>
2025-08-22 00:30:22 +0200

    feat: add header

--- app/components/ThemeSwitcher.vue
+++ app/components/ThemeSwitcher.vue
@@ -1,11 +1,54 @@
 <template>
-  <button @click="toggleTheme()" class="bg-gray-300">changer theme</button>
+  <button
+    @click="toggleTheme()"
+    class="flex p-1 rounded-md hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors">
+    <svg
+      v-if="isDarkModeEnabled === true"
+      class="w-5 h-5 dark:fill-gray-100"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M12.0557 3.59974C12.2752 3.2813 12.2913 2.86484 12.0972 2.53033C11.9031 2.19582 11.5335 2.00324 11.1481 2.03579C6.02351 2.46868 2 6.76392 2 12C2 17.5228 6.47715 22 12 22C17.236 22 21.5313 17.9764 21.9642 12.8518C21.9967 12.4664 21.8041 12.0968 21.4696 11.9027C21.1351 11.7086 20.7187 11.7248 20.4002 11.9443C19.4341 12.6102 18.2641 13 17 13C13.6863 13 11 10.3137 11 6.99996C11 5.73589 11.3898 4.56587 12.0557 3.59974Z"></path>
+      </g>
+    </svg>
+
+    <svg
+      v-if="isDarkModeEnabled === false"
+      class="w-5 h-5 fill-gray-400 stroke-gray-400"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <g clip-path="url(#a)">
+          <path
+            d="M12 0a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0V1a1 1 0 0 1 1-1ZM0 12a1 1 0 0 1 1-1h2a1 1 0 1 1 0 2H1a1 1 0 0 1-1-1ZM21 11a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2ZM13 21a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0v-2ZM6.343 17.657a1 1 0 0 1 0 1.414L4.93 20.485a1 1 0 1 1-1.414-1.414l1.414-1.414a1 1 0 0 1 1.414 0ZM20.485 3.515a1 1 0 0 1 0 1.414l-1.414 1.414a1 1 0 1 1-1.414-1.414l1.414-1.414a1 1 0 0 1 1.414 0ZM3.515 3.515a1 1 0 0 1 1.414 0l1.414 1.414A1 1 0 1 1 4.93 6.343L3.515 4.93a1 1 0 0 1 0-1.414ZM17.657 17.657a1 1 0 0 1 1.414 0l1.414 1.414a1 1 0 1 1-1.414 1.414l-1.414-1.414a1 1 0 0 1 0-1.414ZM5 12a7 7 0 1 1 14 0 7 7 0 0 1-14 0Z"></path>
+        </g>
+        <defs>
+          <clipPath id="a">
+            <path fill="#ffffff" d="M0 0h24v24H0z"></path>
+          </clipPath>
+        </defs>
+      </g>
+    </svg>
+  </button>
 </template>
 
 <script setup>
-let isDarkModeEnabled = true;
+let isDarkModeEnabled = ref(true);
 function toggleTheme() {
-  isDarkModeEnabled = !isDarkModeEnabled;
-  document.documentElement.classList.toggle("dark", isDarkModeEnabled);
+  isDarkModeEnabled.value = !isDarkModeEnabled.value;
+  document.documentElement.classList.toggle("dark", isDarkModeEnabled.value);
 }
 </script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex justify-center items-center w-screen min-h-screen bg-violet-50 dark:bg-gray-950">
+    class="flex justify-center items-center max-w-screen min-h-screen bg-gray-100 dark:bg-gray-950">
     <span class="text-3xl text-gray-600 dark:text-gray-100">
       Travaux en cours 🚧...
     </span>
